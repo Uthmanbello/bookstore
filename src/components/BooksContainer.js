a4554526ffc2bf52dd2a4d15ec27b494b4f5bfb8
Uthmanbello <uthmanbello88@gmail.com>
2023-03-04 00:35:51 +0100

    Connect the redux to api

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,22 +1,40 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
 import Form from './Form';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 const Books = () => {
-  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const ifSucceed = useSelector((store) => (store.books.ifSucceed));
+  const books = useSelector((store) => store.books.books);
+  const isLoading = useSelector((store) => store.books.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [ifSucceed, dispatch]);
+
+  let content;
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (ifSucceed) {
+    content = Object.keys(books).map((key) => {
+      const currentBook = books[key][0];
+      return (
+        <Book
+          key={key}
+          id={key}
+          title={currentBook.title}
+          author={currentBook.author}
+        />
+      );
+    });
+  }
+
   return (
     <section>
-      <h1>BOOKS AND AUTHORS</h1>
-      <ul>
-        {books.map((book) => (
-          <Book
-            key={book.item_id}
-            id={book.item_id}
-            title={book.title}
-            author={book.author}
-          />
-        ))}
-      </ul>
+      <h1>List of books</h1>
+      <ul>{content}</ul>
       <Form />
     </section>
   );
